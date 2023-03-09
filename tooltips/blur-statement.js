70fc4bb98a72901a9df63daddaf97a523f5861b5
lifelabslearning <122063510+lifelabslearning@users.noreply.github.com>
2023-03-08 23:58:03 -0800

    Update blur-statement.js

--- tooltips/blur-statement.js
+++ tooltips/blur-statement.js
@@ -122,86 +122,86 @@ const blur_statement = document.getElementById("blur_statement");
 
 blur_statement.innerHTML = `
 <div id="quote-1" class="quote">
-<p id="blur-hidden" class="quote-body" aria-label="Quote text">
-  Your messages are confusing, I'd prefer if you'd be more direct.
-</p>
-<p id="blur-visible" class="quote-body" aria-label="Quote text">
-  <span
-    class="tooltip"
-    tabindex="0"
-    aria-label="Text or word with tooltip"
-    >Your messages<span class="quote-body">
-      <span class="container"
-        ><span class="text" aria-label="Text or word description"
-          >Which message? All messages? A particular topic? The lack of
-          specificity means the recipient not only doesn't know where the
-          problem is, they are also likely to get defensive. <br /><br />
-          <b>To Deblur:</b> “Your messages” &#8594; “Your email updates on
-          Project X"
-        </span></span
-      ></span
-    ></span
-  >&nbsp;are
-  <span
-    class="tooltip"
-    tabindex="0"
-    aria-label="Text or word with tooltip"
-    >confusing,<span class="quote-body"
-      ><span class="container"
-        ><span class="text" aria-label="Text or word description"
-          >What is confusing about them? Are they too short/long?
-          Formatted in a way that's hard to read? Delivered to the wrong
-          people?<br />
-          <br /><b>To Deblur:</b> Being clear here will make it more
-          likely the other person will make a fix that solves the issue.
-          “confusing” &#8594; “do not have a clear next step.</span
-        ></span
-      ></span
-    ></span
-  >&nbsp;I'd prefer if you'd be more
-  <span
-    class="tooltip"
-    tabindex="0"
-    aria-label="Text or word with tooltip"
-    >direct.<span class="quote-body"
-      ><span class="container"
-        ><span class="text" aria-label="Text or word description"
-          >Concepts like “direct” and “indirect” are subjective person to
-          person and culture to culture.<br /><br />
-          <b>To Deblur:</b> If there's something specific you'd like to
-          see, name it. “more direct” &#8594; “you'd include the action
-          and the person you'd like to own the next step.</span
-        ></span
-      ></span
-    ></span
-  >
-</p>
-<div style="padding-top: 2rem">
-  <button
-    type="button"
-    id="blur-button"
-    aria-label="Show blur words"
-    onclick="showBlurWords()"
-  >
-    Show blur words
-  </button>
-  <p id="instructions">
-    Select a blur word to learn more
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke-width="2"
-      stroke="currentColor"
-      style="height: 1.2rem"
-    >
-      <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        d="M4.5 10.5L12 3m0 0l7.5 7.5M12 3v18"
-      />
-    </svg>
-  </p>
-</div>
-</div>
+      <p id="blur-hidden" class="quote-body" aria-label="Quote text">
+        "Your messages are confusing, I'd prefer if you'd be more direct."
+      </p>
+      <p id="blur-visible" class="quote-body" aria-label="Quote text">
+        <span
+          class="tooltip"
+          tabindex="0"
+          aria-label="Text or word with tooltip"
+          >"Your messages<span class="quote-body">
+            <span class="container"
+              ><span class="text" aria-label="Text or word description"
+                >Which message? All messages? A particular topic? The lack of
+                specificity means the recipient not only doesn't know where the
+                problem is, they are also likely to get defensive. <br /><br />
+                <b>To Deblur:</b> “Your messages” &#8594; “Your email updates on
+                Project X"
+              </span></span
+            ></span
+          ></span
+        >&nbsp;are
+        <span
+          class="tooltip"
+          tabindex="0"
+          aria-label="Text or word with tooltip"
+          >confusing,<span class="quote-body"
+            ><span class="container"
+              ><span class="text" aria-label="Text or word description"
+                >What is confusing about them? Are they too short/long?
+                Formatted in a way that's hard to read? Delivered to the wrong
+                people?<br />
+                <br /><b>To Deblur:</b> Being clear here will make it more
+                likely the other person will make a fix that solves the issue.
+                “confusing” &#8594; “do not have a clear next step.</span
+              ></span
+            ></span
+          ></span
+        >&nbsp;I'd prefer if you'd be more
+        <span
+          class="tooltip"
+          tabindex="0"
+          aria-label="Text or word with tooltip"
+          >direct."<span class="quote-body"
+            ><span class="container"
+              ><span class="text" aria-label="Text or word description"
+                >Concepts like “direct” and “indirect” are subjective person to
+                person and culture to culture.<br /><br />
+                <b>To Deblur:</b> If there's something specific you'd like to
+                see, name it. “more direct” &#8594; “you'd include the action
+                and the person you'd like to own the next step.</span
+              ></span
+            ></span
+          ></span
+        >
+      </p>
+      <div style="padding-top: 2rem">
+        <button
+          type="button"
+          id="blur-button"
+          aria-label="Show blur words"
+          onclick="showBlurWords()"
+        >
+          Show blur words
+        </button>
+        <p id="instructions">
+          Select a blur word to learn more
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="2"
+            stroke="currentColor"
+            style="height: 1.2rem"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M4.5 10.5L12 3m0 0l7.5 7.5M12 3v18"
+            />
+          </svg>
+        </p>
+      </div>
+    </div>
     `;
