22a97ba69a433b698c64da65ad75a8d72f4e4601
lifelabslearning <122063510+lifelabslearning@users.noreply.github.com>
2023-09-19 09:43:28 -0700

    Update avatar image to svg

--- chat-bubbles/impact-statements/impact-statements.js
+++ chat-bubbles/impact-statements/impact-statements.js
@@ -78,13 +78,9 @@ impact_manager.innerHTML = `
         <div class="chat-row me message" style="--delay: 3s">
           <p class="send">Yes, please!</p>
           <div class="avatar">
-            <img
-              src="https://s3.amazonaws.com/outseta-production/7851/0-Ian-Scott-headshot-300x380_b632e9d3-7798-4d20-b509-c85e83079149.jpeg"
-              loading="lazy"
-              data-o-member="ProfileImageS3Url"
-              alt="profile image"
-              onerror="this.src='https://uploads-ssl.webflow.com/63d2ab48da2a2f57c52158d4/63d2ab48da2a2f97be21593c_blank.png'"
-            />
+          <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" style="margin: -7px;">
+          <path d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM12 5C13.66 5 15 6.34 15 8C15 9.66 13.66 11 12 11C10.34 11 9 9.66 9 8C9 6.34 10.34 5 12 5ZM12 19.2C9.5 19.2 7.29 17.92 6 15.98C6.03 13.99 10 12.9 12 12.9C13.99 12.9 17.97 13.99 18 15.98C16.71 17.92 14.5 19.2 12 19.2Z" fill="currentColor"></path>
+          </svg>
           </div>
         </div>
 
@@ -168,13 +164,9 @@ impact_manager.innerHTML = `
         <div class="chat-row me message" style="--delay: 6s">
           <p class="send">Yes, please!</p>
           <div class="avatar">
-            <img
-              src="https://s3.amazonaws.com/outseta-production/7851/0-Ian-Scott-headshot-300x380_b632e9d3-7798-4d20-b509-c85e83079149.jpeg"
-              loading="lazy"
-              data-o-member="ProfileImageS3Url"
-              alt="profile image"
-              onerror="this.src='https://uploads-ssl.webflow.com/63d2ab48da2a2f57c52158d4/63d2ab48da2a2f97be21593c_blank.png'"
-            />
+            <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" style="margin: -7px;">
+          <path d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM12 5C13.66 5 15 6.34 15 8C15 9.66 13.66 11 12 11C10.34 11 9 9.66 9 8C9 6.34 10.34 5 12 5ZM12 19.2C9.5 19.2 7.29 17.92 6 15.98C6.03 13.99 10 12.9 12 12.9C13.99 12.9 17.97 13.99 18 15.98C16.71 17.92 14.5 19.2 12 19.2Z" fill="currentColor"></path>
+          </svg>
           </div>
         </div>
 
@@ -255,13 +247,9 @@ impact_peer.innerHTML = `
         <div class="chat-row me message" style="--delay: 3s">
           <p class="send">Yes, please!</p>
           <div class="avatar">
-            <img
-              src="https://s3.amazonaws.com/outseta-production/7851/0-Ian-Scott-headshot-300x380_b632e9d3-7798-4d20-b509-c85e83079149.jpeg"
-              loading="lazy"
-              data-o-member="ProfileImageS3Url"
-              alt="profile image"
-              onerror="this.src='https://uploads-ssl.webflow.com/63d2ab48da2a2f57c52158d4/63d2ab48da2a2f97be21593c_blank.png'"
-            />
+            <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" style="margin: -7px;">
+          <path d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM12 5C13.66 5 15 6.34 15 8C15 9.66 13.66 11 12 11C10.34 11 9 9.66 9 8C9 6.34 10.34 5 12 5ZM12 19.2C9.5 19.2 7.29 17.92 6 15.98C6.03 13.99 10 12.9 12 12.9C13.99 12.9 17.97 13.99 18 15.98C16.71 17.92 14.5 19.2 12 19.2Z" fill="currentColor"></path>
+          </svg>
           </div>
         </div>
 
@@ -344,13 +332,9 @@ impact_peer.innerHTML = `
         <div class="chat-row me message" style="--delay: 6s">
           <p class="send">Yes, please!</p>
           <div class="avatar">
-            <img
-              src="https://s3.amazonaws.com/outseta-production/7851/0-Ian-Scott-headshot-300x380_b632e9d3-7798-4d20-b509-c85e83079149.jpeg"
-              loading="lazy"
-              data-o-member="ProfileImageS3Url"
-              alt="profile image"
-              onerror="this.src='https://uploads-ssl.webflow.com/63d2ab48da2a2f57c52158d4/63d2ab48da2a2f97be21593c_blank.png'"
-            />
+            <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" style="margin: -7px;">
+          <path d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM12 5C13.66 5 15 6.34 15 8C15 9.66 13.66 11 12 11C10.34 11 9 9.66 9 8C9 6.34 10.34 5 12 5ZM12 19.2C9.5 19.2 7.29 17.92 6 15.98C6.03 13.99 10 12.9 12 12.9C13.99 12.9 17.97 13.99 18 15.98C16.71 17.92 14.5 19.2 12 19.2Z" fill="currentColor"></path>
+          </svg>
           </div>
         </div>
 
@@ -433,13 +417,9 @@ impact_report.innerHTML = `
         <div class="chat-row me message" style="--delay: 3s">
           <p class="send">Yes, please!</p>
           <div class="avatar">
-            <img
-              src="https://s3.amazonaws.com/outseta-production/7851/0-Ian-Scott-headshot-300x380_b632e9d3-7798-4d20-b509-c85e83079149.jpeg"
-              loading="lazy"
-              data-o-member="ProfileImageS3Url"
-              alt="profile image"
-              onerror="this.src='https://uploads-ssl.webflow.com/63d2ab48da2a2f57c52158d4/63d2ab48da2a2f97be21593c_blank.png'"
-            />
+            <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" style="margin: -7px;">
+          <path d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM12 5C13.66 5 15 6.34 15 8C15 9.66 13.66 11 12 11C10.34 11 9 9.66 9 8C9 6.34 10.34 5 12 5ZM12 19.2C9.5 19.2 7.29 17.92 6 15.98C6.03 13.99 10 12.9 12 12.9C13.99 12.9 17.97 13.99 18 15.98C16.71 17.92 14.5 19.2 12 19.2Z" fill="currentColor"></path>
+          </svg>
           </div>
         </div>
 
@@ -522,13 +502,9 @@ impact_report.innerHTML = `
         <div class="chat-row me message" style="--delay: 6s">
           <p class="send">Yes, please!</p>
           <div class="avatar">
-            <img
-              src="https://s3.amazonaws.com/outseta-production/7851/0-Ian-Scott-headshot-300x380_b632e9d3-7798-4d20-b509-c85e83079149.jpeg"
-              loading="lazy"
-              data-o-member="ProfileImageS3Url"
-              alt="profile image"
-              onerror="this.src='https://uploads-ssl.webflow.com/63d2ab48da2a2f57c52158d4/63d2ab48da2a2f97be21593c_blank.png'"
-            />
+            <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" style="margin: -7px;">
+          <path d="M12 2C6.48 2 2 6.48 2 12C2 17.52 6.48 22 12 22C17.52 22 22 17.52 22 12C22 6.48 17.52 2 12 2ZM12 5C13.66 5 15 6.34 15 8C15 9.66 13.66 11 12 11C10.34 11 9 9.66 9 8C9 6.34 10.34 5 12 5ZM12 19.2C9.5 19.2 7.29 17.92 6 15.98C6.03 13.99 10 12.9 12 12.9C13.99 12.9 17.97 13.99 18 15.98C16.71 17.92 14.5 19.2 12 19.2Z" fill="currentColor"></path>
+          </svg>
           </div>
         </div>
 
