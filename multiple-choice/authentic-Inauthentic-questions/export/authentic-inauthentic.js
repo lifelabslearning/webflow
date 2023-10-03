05955e4b576ee04b77e216c0e5cb7f038cbb91bc
alexdemichieli <alexdemichieli@github.com>
2023-10-02 23:17:56 -0700

    changing order of questions in lesson 3

--- multiple-choice/authentic-Inauthentic-questions/export/authentic-inauthentic.js
+++ multiple-choice/authentic-Inauthentic-questions/export/authentic-inauthentic.js
@@ -77,8 +77,45 @@ authentic_1.innerHTML = `
 authentic_2.innerHTML = `
 <div class="grid-two-cards">
 <label class="card">
+  <input name="options-b" class="radio" type="radio" />
+  <span class="option">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      class="warning symbol"
+    >
+      <path
+        fill-rule="evenodd"
+        d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zM12 8.25a.75.75 0 01.75.75v3.75a.75.75 0 01-1.5 0V9a.75.75 0 01.75-.75zm0 8.25a.75.75 0 100-1.5.75.75 0 000 1.5z"
+        clip-rule="evenodd"
+      />
+    </svg>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      class="check symbol"
+    >
+      <path
+        fill-rule="evenodd"
+        d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
+        clip-rule="evenodd"
+      />
+    </svg>
+    <span class="answer"
+      >Don’t you think the new process is much better than the previous
+      one?
+    </span>
+    <span class="feedback">
+      Try Again. This is a leading question that already presumes The new
+      system is better than the old one, so there’s no real curiosity.
+    </span>
+  </span>
+</label>
+<label class="card">
   <input name="options-b" class="radio correct" type="radio" />
-  <span class="option two-cards-option">
+  <span class="option">
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
@@ -112,9 +149,14 @@ authentic_2.innerHTML = `
     </span>
   </span>
 </label>
+</div>
+`;
+
+authentic_3.innerHTML = `
+<div class="grid-two-cards">
 <label class="card">
-  <input name="options-b" class="radio" type="radio" />
-  <span class="option two-cards-option">
+  <input name="options-c" class="radio" type="radio" />
+  <span class="option">
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
@@ -140,23 +182,17 @@ authentic_2.innerHTML = `
       />
     </svg>
     <span class="answer"
-      >Don’t you think the new process is much better than the
-      previous one?
-    </span>
+      >Isn’t it best to ask your manager to weigh in?</span
+    >
     <span class="feedback">
-      Try Again. This is a leading question that already presumes The new
-      system is better than the old one, so there’s no real curiosity.
+      Try Again: This question reveals the opinion of the person asking
+      the question rather than creating space for a dialogue.
     </span>
   </span>
 </label>
-</div>
-`;
-
-authentic_3.innerHTML = `
-<div class="grid-two-cards">
 <label class="card">
   <input name="options-c" class="radio correct" type="radio" />
-  <span class="option two-cards-option">
+  <span class="option">
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
@@ -181,42 +217,12 @@ authentic_3.innerHTML = `
         clip-rule="evenodd"
       />
     </svg>
-    <span class="answer">What support do you need from your manager?</span>
-    <span class="feedback">
-      Correct! This open question invites the responder to think about their needs and the kind of support they would like to receive.
-    </span>
-  </span>
-</label>
-<label class="card">
-  <input name="options-c" class="radio" type="radio" />
-  <span class="option two-cards-option">
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="currentColor"
-      class="warning symbol"
+    <span class="answer"
+      >What support do you need from your manager?</span
     >
-      <path
-        fill-rule="evenodd"
-        d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zM12 8.25a.75.75 0 01.75.75v3.75a.75.75 0 01-1.5 0V9a.75.75 0 01.75-.75zm0 8.25a.75.75 0 100-1.5.75.75 0 000 1.5z"
-        clip-rule="evenodd"
-      />
-    </svg>
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="currentColor"
-      class="check symbol"
-    >
-      <path
-        fill-rule="evenodd"
-        d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
-        clip-rule="evenodd"
-      />
-    </svg>
-    <span class="answer">Isn’t it best to ask your manager to weigh in?</span>
     <span class="feedback">
-      Try Again: This question reveals the opinion of the person asking the question rather than creating space for a dialogue.
+      Correct! This open question invites the responder to think about
+      their needs and the kind of support they would like to receive.
     </span>
   </span>
 </label>
