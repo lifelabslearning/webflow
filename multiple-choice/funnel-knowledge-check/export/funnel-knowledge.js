894116644e8a541cf4600f6bf5226d590bc0b002
alexdemichieli <alexdemichieli@github.com>
2023-10-02 23:45:17 -0700

    changed order of cards for funnel knowledge

--- multiple-choice/funnel-knowledge-check/export/funnel-knowledge.js
+++ multiple-choice/funnel-knowledge-check/export/funnel-knowledge.js
@@ -6,6 +6,40 @@ const q4 = document.querySelector("#q4");
 q1.innerHTML = `
 <div class="grid-four-cards">
 <label class="card">
+  <input name="options-a" class="radio correct" type="radio" />
+  <span class="option">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      class="warning symbol"
+    >
+      <path
+        fill-rule="evenodd"
+        d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zM12 8.25a.75.75 0 01.75.75v3.75a.75.75 0 01-1.5 0V9a.75.75 0 01.75-.75zm0 8.25a.75.75 0 100-1.5.75.75 0 000 1.5z"
+        clip-rule="evenodd"
+      />
+    </svg>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      class="check symbol"
+    >
+      <path
+        fill-rule="evenodd"
+        d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
+        clip-rule="evenodd"
+      />
+    </svg>
+    <span class="answer">Success</span>
+    <span class="feedback">
+      Correct! The keyword is goal. A goal is designed to help you
+      identify your desired state.
+    </span>
+  </span>
+</label>
+<label class="card">
   <input name="options-a" class="radio" type="radio" />
   <span class="option">
     <svg
@@ -32,9 +66,10 @@ q1.innerHTML = `
         clip-rule="evenodd"
       />
     </svg>
-    <span class="answer">Next Steps</span>
+    <span class="answer">Obstacles</span>
     <span class="feedback">
-      Try again: Next steps are actions towards a goal or desired state.
+      Try again: An obstacle is something that gets in the way of a
+      desired goal or state.
     </span>
   </span>
 </label>
@@ -68,7 +103,8 @@ q1.innerHTML = `
     <span class="answer">Options</span>
 
     <span class="feedback">
-      Try again: Options are different ways of approaching or solving a problem.
+      Try again: Options are different ways of approaching or solving a
+      problem.
     </span>
   </span>
 </label>
@@ -99,42 +135,9 @@ q1.innerHTML = `
         clip-rule="evenodd"
       />
     </svg>
-    <span class="answer">Obstacles</span>
+    <span class="answer">Next Steps</span>
     <span class="feedback">
-      Try again: An obstacle is something that gets in the way of a desired goal or state.
-    </span>
-  </span>
-</label>
-<label class="card">
-  <input name="options-a" class="radio correct" type="radio" />
-  <span class="option">
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="currentColor"
-      class="warning symbol"
-    >
-      <path
-        fill-rule="evenodd"
-        d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zM12 8.25a.75.75 0 01.75.75v3.75a.75.75 0 01-1.5 0V9a.75.75 0 01.75-.75zm0 8.25a.75.75 0 100-1.5.75.75 0 000 1.5z"
-        clip-rule="evenodd"
-      />
-    </svg>
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="currentColor"
-      class="check symbol"
-    >
-      <path
-        fill-rule="evenodd"
-        d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
-        clip-rule="evenodd"
-      />
-    </svg>
-    <span class="answer">Success</span>
-    <span class="feedback">
-      Correct! The keyword is goal. A goal is designed to help you identify your desired state.
+      Try again: Next steps are actions towards a goal or desired state.
     </span>
   </span>
 </label>
@@ -170,9 +173,44 @@ q2.innerHTML = `
         clip-rule="evenodd"
       />
     </svg>
-    <span class="answer">Next Steps</span>
+    <span class="answer">Success</span>
     <span class="feedback">
-      Try again: Next steps are actions towards a goal or desired state.
+      Try again: Success refers to a goal designed to help you identify
+      your desired state.
+    </span>
+  </span>
+</label>
+<label class="card">
+  <input name="options-b" class="radio correct" type="radio" />
+  <span class="option">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      class="warning symbol"
+    >
+      <path
+        fill-rule="evenodd"
+        d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zM12 8.25a.75.75 0 01.75.75v3.75a.75.75 0 01-1.5 0V9a.75.75 0 01.75-.75zm0 8.25a.75.75 0 100-1.5.75.75 0 000 1.5z"
+        clip-rule="evenodd"
+      />
+    </svg>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      class="check symbol"
+    >
+      <path
+        fill-rule="evenodd"
+        d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
+        clip-rule="evenodd"
+      />
+    </svg>
+    <span class="answer">Obstacles</span>
+    <span class="feedback">
+      Correct! The keyword is concern. A concern is something that gets in
+      the way of your desired goal or state.
     </span>
   </span>
 </label>
@@ -206,40 +244,9 @@ q2.innerHTML = `
     <span class="answer">Options</span>
 
     <span class="feedback">
-      Try again: Options are different ways of approaching or solving a problem.
+      Try again: Options are different ways of approaching or solving a
+      problem.
     </span>
-  </span>
-</label>
-<label class="card">
-  <input name="options-b" class="radio correct" type="radio" />
-  <span class="option">
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="currentColor"
-      class="warning symbol"
-    >
-      <path
-        fill-rule="evenodd"
-        d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zM12 8.25a.75.75 0 01.75.75v3.75a.75.75 0 01-1.5 0V9a.75.75 0 01.75-.75zm0 8.25a.75.75 0 100-1.5.75.75 0 000 1.5z"
-        clip-rule="evenodd"
-      />
-    </svg>
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="currentColor"
-      class="check symbol"
-    >
-      <path
-        fill-rule="evenodd"
-        d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
-        clip-rule="evenodd"
-      />
-    </svg>
-    <span class="answer">Obstacles</span>
-    <span class="feedback">
-      Correct! The keyword is concern. A concern is something that gets in the way of your desired goal or state.          </span>
   </span>
 </label>
 <label class="card">
@@ -269,9 +276,9 @@ q2.innerHTML = `
         clip-rule="evenodd"
       />
     </svg>
-    <span class="answer">Success</span>
+    <span class="answer">Next Steps</span>
     <span class="feedback">
-      Try again: Success refers to a goal designed to help you identify your desired state.
+      Try again: Next steps are actions towards a goal or desired state.
     </span>
   </span>
 </label>
@@ -307,9 +314,44 @@ q3.innerHTML = `
         clip-rule="evenodd"
       />
     </svg>
-    <span class="answer">Next Steps</span>
+    <span class="answer">Success</span>
     <span class="feedback">
-      Try again: Next steps are actions towards a goal or desired state.
+      Try again: Success refers to a goal designed to help you identify
+      your desired state.
+    </span>
+  </span>
+</label>
+<label class="card">
+  <input name="options-c" class="radio" type="radio" />
+  <span class="option">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      class="warning symbol"
+    >
+      <path
+        fill-rule="evenodd"
+        d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zM12 8.25a.75.75 0 01.75.75v3.75a.75.75 0 01-1.5 0V9a.75.75 0 01.75-.75zm0 8.25a.75.75 0 100-1.5.75.75 0 000 1.5z"
+        clip-rule="evenodd"
+      />
+    </svg>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      class="check symbol"
+    >
+      <path
+        fill-rule="evenodd"
+        d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
+        clip-rule="evenodd"
+      />
+    </svg>
+    <span class="answer">Obstacles</span>
+    <span class="feedback">
+      Try again: An obstacle is something that gets in the way of a
+      desired goal or state.
     </span>
   </span>
 </label>
@@ -343,83 +385,13 @@ q3.innerHTML = `
     <span class="answer">Options</span>
 
     <span class="feedback">
-      Correct! The key word is possibilities. Options are different ways of approaching or solving a problem.
+      Correct! The key word is possibilities. Options are different ways
+      of approaching or solving a problem.
     </span>
   </span>
 </label>
 <label class="card">
   <input name="options-c" class="radio" type="radio" />
-  <span class="option">
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="currentColor"
-      class="warning symbol"
-    >
-      <path
-        fill-rule="evenodd"
-        d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zM12 8.25a.75.75 0 01.75.75v3.75a.75.75 0 01-1.5 0V9a.75.75 0 01.75-.75zm0 8.25a.75.75 0 100-1.5.75.75 0 000 1.5z"
-        clip-rule="evenodd"
-      />
-    </svg>
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="currentColor"
-      class="check symbol"
-    >
-      <path
-        fill-rule="evenodd"
-        d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
-        clip-rule="evenodd"
-      />
-    </svg>
-    <span class="answer">Obstacles</span>
-    <span class="feedback">
-      Try again: An obstacle is something that gets in the way of a desired goal or state.
-    </span>
-  </span>
-</label>
-<label class="card">
-  <input name="options-c" class="radio" type="radio" />
-  <span class="option">
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="currentColor"
-      class="warning symbol"
-    >
-      <path
-        fill-rule="evenodd"
-        d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zM12 8.25a.75.75 0 01.75.75v3.75a.75.75 0 01-1.5 0V9a.75.75 0 01.75-.75zm0 8.25a.75.75 0 100-1.5.75.75 0 000 1.5z"
-        clip-rule="evenodd"
-      />
-    </svg>
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="currentColor"
-      class="check symbol"
-    >
-      <path
-        fill-rule="evenodd"
-        d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
-        clip-rule="evenodd"
-      />
-    </svg>
-    <span class="answer">Success</span>
-    <span class="feedback">
-      Try again: Success refers to a goal designed to help you identify your desired state.
-    </span>
-  </span>
-</label>
-</div>
-`;
-
-q4.innerHTML = `
-<div class="grid-four-cards">
-<label class="card">
-  <input name="options-d" class="radio correct" type="radio" />
   <span class="option">
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -447,7 +419,80 @@ q4.innerHTML = `
     </svg>
     <span class="answer">Next Steps</span>
     <span class="feedback">
-      Correct! The keyword is moving. Identifying next steps helps you determine actions towards a goal or desired state.
+      Try again: Next steps are actions towards a goal or desired state.
+    </span>
+  </span>
+</label>
+</div>
+`;
+
+q4.innerHTML = `
+<div class="grid-four-cards">
+<label class="card">
+  <input name="options-d" class="radio" type="radio" />
+  <span class="option">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      class="warning symbol"
+    >
+      <path
+        fill-rule="evenodd"
+        d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zM12 8.25a.75.75 0 01.75.75v3.75a.75.75 0 01-1.5 0V9a.75.75 0 01.75-.75zm0 8.25a.75.75 0 100-1.5.75.75 0 000 1.5z"
+        clip-rule="evenodd"
+      />
+    </svg>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      class="check symbol"
+    >
+      <path
+        fill-rule="evenodd"
+        d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
+        clip-rule="evenodd"
+      />
+    </svg>
+    <span class="answer">Success</span>
+    <span class="feedback">
+      Try again: Success refers to a goal designed to help you identify
+      your desired state.
+    </span>
+  </span>
+</label>
+<label class="card">
+  <input name="options-d" class="radio" type="radio" />
+  <span class="option">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      class="warning symbol"
+    >
+      <path
+        fill-rule="evenodd"
+        d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zM12 8.25a.75.75 0 01.75.75v3.75a.75.75 0 01-1.5 0V9a.75.75 0 01.75-.75zm0 8.25a.75.75 0 100-1.5.75.75 0 000 1.5z"
+        clip-rule="evenodd"
+      />
+    </svg>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      class="check symbol"
+    >
+      <path
+        fill-rule="evenodd"
+        d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
+        clip-rule="evenodd"
+      />
+    </svg>
+    <span class="answer">Obstacles</span>
+    <span class="feedback">
+      Try again: An obstacle is something that gets in the way of a
+      desired goal or state.
     </span>
   </span>
 </label>
@@ -481,12 +526,13 @@ q4.innerHTML = `
     <span class="answer">Options</span>
 
     <span class="feedback">
-      Try again: Options are different ways of approaching or solving a problem.
+      Try again: Options are different ways of approaching or solving a
+      problem.
     </span>
   </span>
 </label>
 <label class="card">
-  <input name="options-d" class="radio" type="radio" />
+  <input name="options-d" class="radio correct" type="radio" />
   <span class="option">
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -512,42 +558,10 @@ q4.innerHTML = `
         clip-rule="evenodd"
       />
     </svg>
-    <span class="answer">Obstacles</span>
+    <span class="answer">Next Steps</span>
     <span class="feedback">
-      Try again: An obstacle is something that gets in the way of a desired goal or state.
-    </span>
-  </span>
-</label>
-<label class="card">
-  <input name="options-d" class="radio" type="radio" />
-  <span class="option">
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="currentColor"
-      class="warning symbol"
-    >
-      <path
-        fill-rule="evenodd"
-        d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zM12 8.25a.75.75 0 01.75.75v3.75a.75.75 0 01-1.5 0V9a.75.75 0 01.75-.75zm0 8.25a.75.75 0 100-1.5.75.75 0 000 1.5z"
-        clip-rule="evenodd"
-      />
-    </svg>
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
-      fill="currentColor"
-      class="check symbol"
-    >
-      <path
-        fill-rule="evenodd"
-        d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
-        clip-rule="evenodd"
-      />
-    </svg>
-    <span class="answer">Success</span>
-    <span class="feedback">
-      Try again: Success refers to a goal designed to help you identify your desired state.
+      Correct! The keyword is moving. Identifying next steps helps you
+      determine actions towards a goal or desired state.
     </span>
   </span>
 </label>
