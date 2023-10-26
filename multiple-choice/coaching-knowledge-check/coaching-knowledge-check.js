4064980a12b4f8e20aa0427ff720a060d176587d
alexdemichieli <alexdemichieli@github.com>
2023-10-26 13:58:42 -0700

    adding changes to q-step cards order

--- multiple-choice/coaching-knowledge-check/coaching-knowledge-check.js
+++ multiple-choice/coaching-knowledge-check/coaching-knowledge-check.js
@@ -114,6 +114,41 @@ quiz_a.innerHTML = `
 
   quiz_b.innerHTML = `
   <div class="grid">
+ 
+  <label class="card">
+    <input name="options-b" class="radio" type="radio" />
+    <span class="option">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="currentColor"
+        class="warning symbol"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zM12 8.25a.75.75 0 01.75.75v3.75a.75.75 0 01-1.5 0V9a.75.75 0 01.75-.75zm0 8.25a.75.75 0 100-1.5.75.75 0 000 1.5z"
+          clip-rule="evenodd"
+        />
+      </svg>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="currentColor"
+        class="check symbol"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
+          clip-rule="evenodd"
+        />
+      </svg>
+      <span class="answer">Split-track </span>
+      <span class="feedback"
+        >Try Again: Split-tracks are a type of playback that can help bring clarity and focus. Split-tracks start very similar to a playback with your repeating back what you heard and clearly identifying each issue when someone brings up more than one issue.
+      </span>
+    </span>
+  </label>
+
   <label class="card">
     <input name="options-b" class="radio" type="radio" />
     <span class="option">
@@ -150,6 +185,7 @@ quiz_a.innerHTML = `
       >
     </span>
   </label>
+
   <label class="card">
     <input name="options-b" class="radio correct" type="radio" />
     <span class="option">
@@ -186,80 +222,12 @@ quiz_a.innerHTML = `
       </span>
     </span>
   </label>
-  <label class="card">
-    <input name="options-b" class="radio" type="radio" />
-    <span class="option">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        fill="currentColor"
-        class="warning symbol"
-      >
-        <path
-          fill-rule="evenodd"
-          d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zM12 8.25a.75.75 0 01.75.75v3.75a.75.75 0 01-1.5 0V9a.75.75 0 01.75-.75zm0 8.25a.75.75 0 100-1.5.75.75 0 000 1.5z"
-          clip-rule="evenodd"
-        />
-      </svg>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        fill="currentColor"
-        class="check symbol"
-      >
-        <path
-          fill-rule="evenodd"
-          d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
-          clip-rule="evenodd"
-        />
-      </svg>
-      <span class="answer">Split-track </span>
-      <span class="feedback"
-        >Try Again: Split-tracks are a type of playback that can help bring clarity and focus. Split-tracks start very similar to a playback with your repeating back what you heard and clearly identifying each issue when someone brings up more than one issue.
-      </span>
-    </span>
-  </label>
 </div>
   `;
 
   quiz_c.innerHTML = `
   <div class="grid">
-  <label class="card">
-    <input name="options-c" class="radio" type="radio" />
-    <span class="option">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        fill="currentColor"
-        class="warning symbol"
-      >
-        <path
-          fill-rule="evenodd"
-          d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zM12 8.25a.75.75 0 01.75.75v3.75a.75.75 0 01-1.5 0V9a.75.75 0 01.75-.75zm0 8.25a.75.75 0 100-1.5.75.75 0 000 1.5z"
-          clip-rule="evenodd"
-        />
-      </svg>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        fill="currentColor"
-        class="check symbol"
-      >
-        <path
-          fill-rule="evenodd"
-          d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
-          clip-rule="evenodd"
-        />
-      </svg>
-      <span class="answer">Q-step</span>
-      <span class="feedback"
-        >Try Again: Q-stepping is the habit of asking questions first in
-        coaching conversations. Use q-stepping to: a. diagnose an underlying
-        issue, b. help others clarify their thinking, and/or c. learn about
-        others' motivations, priorities, or challenges.</span
-      >
-    </span>
-  </label>
+
   <label class="card">
     <input name="options-c" class="radio" type="radio" />
     <span class="option">
@@ -296,6 +264,46 @@ quiz_a.innerHTML = `
       </span>
     </span>
   </label>
+  
+  <label class="card">
+    <input name="options-c" class="radio" type="radio" />
+    <span class="option">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="currentColor"
+        class="warning symbol"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zM12 8.25a.75.75 0 01.75.75v3.75a.75.75 0 01-1.5 0V9a.75.75 0 01.75-.75zm0 8.25a.75.75 0 100-1.5.75.75 0 000 1.5z"
+          clip-rule="evenodd"
+        />
+      </svg>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="currentColor"
+        class="check symbol"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
+          clip-rule="evenodd"
+        />
+      </svg>
+      <span class="answer">Q-step</span>
+      <span class="feedback"
+        >Try Again: Q-stepping is the habit of asking questions first in
+        coaching conversations. Use q-stepping to: a. diagnose an underlying
+        issue, b. help others clarify their thinking, and/or c. learn about
+        others' motivations, priorities, or challenges.</span
+      >
+    </span>
+  </label>
+
+ 
+
   <label class="card">
     <input name="options-c" class="radio correct" type="radio" />
     <span class="option">
